058f2b5f4f53adbeb7ae9c889d0be9d955254ecb
Rodrigo <ramos03rodrigo@gmail.com>
2023-02-19 20:15:00 +0000

    done:cpp6

---
+++ cpp-module-06/ex02/identify.hpp
@@ -0,0 +1,26 @@
+#ifndef IDENTIFY_HPP
+#define IDENTIFY_HPP
+
+#include <stdlib.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class Base
+{
+public:
+	virtual ~Base(){};
+};
+
+class A : public Base
+{
+};
+class B : public Base
+{
+};
+class C : public Base
+{
+};
+
+#endif
